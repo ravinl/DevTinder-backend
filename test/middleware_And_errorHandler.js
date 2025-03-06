3172f9c52be20e4de509525e49d4ce42437df1c1
Ravi <ravi.raushan.nl@gmail.com>
2025-03-06 14:42:32 +0530

    Add: first route handler for '/route'

--- test/middleware_And_errorHandler.js
+++ test/middleware_And_errorHandler.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.use("/route", (req, res) => {
+  // this function is called route handler
+
+  res.send("Hello from the server!!");
+});
+
 app.listen(19777, () => {
   console.log("Server is listening successfully on port 19777");
 });
