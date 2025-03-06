b87e2893f20f516d1ef66dd0ad1b0726c48f7f6f
Ravi <ravi.raushan.nl@gmail.com>
2025-03-06 15:27:36 +0530

    Add: multiple route handler for '/test'

--- test/middleware_And_errorHandler.js
+++ test/middleware_And_errorHandler.js
@@ -16,7 +16,18 @@ app.use(
   (req, res) => {
     res.send("Hello from second user router handler!!!");
   }
-);
+); // for this path '/user' the first route handler will execute.
+
+app.use(
+  "/test",
+  (req, res) => {
+    console.log("1st test route handler");
+  },
+  (req, res) => {
+    console.log("2nd test route handler");
+    res.send("Hello from the test server-2");
+  }
+); // for this path '/test' the first route handler will execute and will be caught in loop because the first route handler is not sending any response.
 
 app.listen(19777, () => {
   console.log("Server is listening successfully on port 19777");
