528921a6cb3244acac933ac1c42ede4b0bf7550b
Ravi <ravi.raushan.nl@gmail.com>
2025-03-09 10:46:24 +0530

    Add: user data for user entry in database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const data_array = require("./data_file.js");
 
 const server = express();
+
+// CRUD Operation
+const devTinder = {
+  name: "DevTinder",
+  developer: "Ravi Raushan Kumar",
+  designer: "Ravi Raushan Kumar",
+  tester: "Ravi Raushan Kumar",
+  created_series: "Namaste Node",
+  mentor: "Akshay Saini",
+  creation_year: 2025,
+  creation_month: "March",
+};
 
 server.listen(5324, () => {
   console.log("Server is listening on port 5324");
