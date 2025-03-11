87684c581443878384b7f0fcd1dcd41bc948b3e1
Ravi <ravi.raushan.nl@gmail.com>
2025-03-11 19:04:49 +0530

    Rename: src/config/listenServer.js to src/config/server.js

---
+++ src/config/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const server = express();
+const PORT = process.env.PORT;
+
+const listenServer = () => {
+  server.listen(PORT, () => {
+    console.log("Server is listening at port", PORT);
+  });
+};
+
+module.exports = { listenServer, server };
