3b4426dfbcd18e2a73a7a280f67020966c6a8822
Ravi <ravi.raushan.nl@gmail.com>
2025-03-11 10:13:55 +0530

    Add: function to connect to mongo database using mongoose

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
 
+const connectDB = async () => {
+  mongoose.connect(
+    "mongodb+srv://raviraushannl:mnhVrwTebxE90Zpx@namastenode.xjqdr.mongodb.net/"
+  );
+};
+
+module.exports = { connectDB };
